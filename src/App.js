68ebe74418dc1cf10bf86981da584b7ca6a6e144
Anna Hittaller <hittaller.anna@gmail.com>
2023-04-17 21:43:50 +0200

    editing of todos and react router added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-
-import './App.css';
-import AddTodo from './components/AddTodo';
-import Calendar from './components/Calendar';
-
+import "./App.css";
+import Calendar from "./components/Calendar";
 import { useState, useEffect } from "react";
-import TodoList from './components/TodoList';
-import Todos from './components/Todos';
-
-import { Header } from './components/Header';
-import { Navigation } from './components/Navigation'
-
-
-//import "./components/Calendar.css";
+import TodoList from "./components/TodoList";
+import Todos from "./components/Todos";
+import { Header } from "./components/Header";
+import { Navigation } from "./components/Navigation";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-	const [todos, setTodos] = useState([]);
+	const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+	const [newTodo, setNewTodo] = useState("");
+	const [todos, setTodos] = useState(storedTodos);
+	const [editTodo, setEditTodo] = useState(null);
 
 	useEffect(() => {
-		const storedTodos = JSON.parse(localStorage.getItem("todos"));
-		if (storedTodos) {
-			setTodos(storedTodos);
-		}
-	}, []);
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
 
 
-  return (
-		<div className="App">
-      <Header />
-			<Calendar />
-			<TodoList todos={todos} setTodos={setTodos} />
-			<Todos todos={todos} setTodos={setTodos} />
-      <Navigation />
-		</div>
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<div className="app-wrapper">
+					<Header />
+					<Routes>
+						<Route path='/calendar' element={<Calendar />} />
+						<Route path='/todolist' element={
+							<TodoList
+								newTodo={newTodo}
+								setNewTodo={setNewTodo}
+								todos={todos}
+								setTodos={setTodos}
+								editTodo={editTodo}
+								setEditTodo={setEditTodo}
+							/>
+						}/>
+						<Route path='/todos' element={
+							<Todos
+								todos={todos}
+								setTodos={setTodos}
+								setEditTodo={setEditTodo}
+							/>
+						}/>
+					</Routes>
+					<Navigation />
+				</div>
+			</div>
+		</BrowserRouter>
 	);
-
 }
 
 export default App;
